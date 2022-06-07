a827588acffe6c86b4de081300d564b9b40aef3a
George Faulkner <gfaulkner675@gmail.com>
2022-06-07 14:20:06 +0100

    initial

---
+++ src/components/nav.svelte
@@ -0,0 +1,27 @@
+<nav class="nav">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/store">Store</a>
+</nav>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap');
+
+    .nav {
+        margin: 0px auto;
+        width: 800px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .nav a {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 600;
+        font-size: 14px;
+        color: black; 
+        text-decoration: none;
+        padding: 0px 20px 0px 20px;
+    }
+
+</style>
