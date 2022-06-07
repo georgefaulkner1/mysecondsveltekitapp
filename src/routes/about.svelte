ca8decf30b0650e08f7c5879fdeded09392daad2
George Faulkner <gfaulkner675@gmail.com>
2022-06-07 15:28:14 +0100

    Added

---
+++ src/routes/about.svelte
@@ -0,0 +1,29 @@
+<script>
+    let title = "About!"
+</script>
+
+<svelte:head>
+    <title>About Prokect</title>
+</svelte:head>
+
+<div class="title-block">
+    <h1 class="title">{title}</h1>
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+    h1 {
+        font-family: 'Pacifico', cursive;
+        color: black;
+        font-size: 42px;
+    }
+
+    .title-block {
+        margin: 0px auto;
+        width: 800px;
+        display: flex;
+        justify-content: center;
+    }
+
+</style>
