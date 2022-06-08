8b880578f0179c3c1f1e6b4680fde257462651bc
George Faulkner <gfaulkner675@gmail.com>
2022-06-08 16:53:06 +0100

    initial

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
     let title = "Just welcome!"
     import { fade } from "svelte/transition"
-
+    import axios from "axios"
+    let name = ""
+/*
     async function loadData(){
         try {
             const url = "https://my-first-express-app-675.herokuapp.com/"
@@ -13,6 +15,20 @@
         }
     }
     loadData()
+*/
+
+    const putLogin = async () => {
+        try {
+            const url = "https://my-first-express-app-675.herokuapp.com/"
+            const res = await axios.put(url, {
+                headers: {},
+                params: {test: false, name}
+            })
+            console.log(res)
+        }catch(err){
+            console.log("ERROR: ", err)
+        }
+    }
 
 </script>
 
@@ -23,6 +39,9 @@
 <div class="title-block">
     <h1 class="title" transition:fade>{title}</h1>
 </div>
+
+<input type="text" bind:value={name} placeholder="Username">
+<button on:click={putLogin}>Login</button>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
